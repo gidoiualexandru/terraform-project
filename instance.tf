b7d1474a26d3f137c7d86f4470d885282ffaf2d8
Alex Gidoiu <gidoiualexandru0@gmail.com>
2025-04-23 16:23:36 +0300

    terraform

---
+++ instance.tf
@@ -0,0 +1,150 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_budgets_budget" "test-budget" {
+  name              = "test-budget"
+  budget_type       = "COST"
+  limit_amount      = "0.01"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2025-04-24_00:01"
+}
+
+resource "aws_vpc" "devops-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.devops-vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.devops-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "public-internet-gateway" {
+  vpc_id = aws_vpc.devops-vpc.id
+
+  tags = {
+    Name = "public-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.devops-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public-internet-gateway.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.devops-vpc.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "subnet-association-private" {
+  subnet_id      = aws_subnet.private-subnet.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "subnet-association-public" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_security_group" "web-server-sg" {
+  name_prefix = "web-server-sg-"
+  vpc_id      = aws_vpc.devops-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["82.76.155.30/32"]
+    description = "Allow SSH inbound"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP inbound"
+  }
+
+  tags = {
+    Name = "web-server-security-group"
+  }
+}
+
+resource "aws_instance" "web-server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public-subnet.id
+  key_name                    = "devops-prod-key"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web-server-sg.id]
+
+  tags = {
+    Name = "web-server"
+  }
+}
